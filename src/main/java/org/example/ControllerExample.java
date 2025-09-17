84898ee9847383c77f8a809580ea9b29ee980d59
Dima Kopfmann <dmitry.golovinov@jetbrains.com>
2025-09-17 14:41:51 +0200

    Proposed change

--- src/main/java/org/example/ControllerExample.java
+++ src/main/java/org/example/ControllerExample.java
@@ -1,6 +1,11 @@
 package org.example;
 
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -8,8 +13,39 @@ import com.sun.net.httpserver.HttpHandler;
 public class ControllerExample implements HttpHandler {
     private Connection connection;
 
-    public void handle(HttpExchange exc) {
+    public void handle(HttpExchange exc) throws IOException {
         String productId = exc.getRequestHeaders().getFirst("productId");
 
+        if (checkIsProductAvailable(productId)) {
+            exc.sendResponseHeaders(200, 0);
+        } else {
+            exc.sendResponseHeaders(403, 0);
+        }
+    }
+
+    public void handle3(HttpExchange exc) throws IOException {
+        String productId = exc.getRequestHeaders().getFirst("productId");
+
+        if (checkIsProductAvailable(productId)) {
+            exc.sendResponseHeaders(200, 0);
+        } else {
+            exc.sendResponseHeaders(403, 0);
+        }
+    }
+
+    private boolean checkIsProductAvailable(String productId) {
+        try (Statement statement = connection.createStatement()) {
+            String sql = "SELECT available FROM products WHERE product_id='" + productId + "'";
+            statement.executeQuery(productId);
+            try (ResultSet resultSet = statement.getResultSet()) {
+                if (resultSet.next()) {
+                    return resultSet.getBoolean("available");
+                }
+                return false;
+            }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
